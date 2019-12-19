7f8de82ba0e8a872c4aa4fc8bd500856e094e3c6
spainhell <gi1982ZL19>
2019-12-19 08:07:36 +0100

    oprava skriptu na stažení počasí

--- scripts/weather.js
+++ scripts/weather.js
@@ -5,22 +5,33 @@ function getFromYrNo() {
         let xmlDoc;
         let parser;
         if (this.readyState == 4 && this.status == 200) {
-            // document.getElementById("demo").innerHTML = this.responseText;
+
             console.log("Response from YR.NO length:", xhttp.responseText.length);
             parser = new DOMParser();
             xmlDoc = parser.parseFromString(xhttp.responseText, "text/xml");
 
-            let location = xmlDoc.getRootNode();
-            let loc2 = location.getElementsByName("location");
-            console.log("location:", loc2)
+            //let root = xmlDoc.getRootNode();
+            let forecast = xmlDoc.getElementsByTagName("forecast")[0];
+            let tabular = forecast.getElementsByTagName("tabular")[0];
+            let time = tabular.getElementsByTagName("time")[0];
+
+            let cloudy = time.getElementsByTagName("symbol")[0];
+            let windDirection = time.getElementsByTagName("windDirection")[0];
+            let windSpeed = time.getElementsByTagName("windSpeed")[0];
+            let temperature = time.getElementsByTagName("temperature")[0];
+            let pressureTag = time.getElementsByTagName("pressure")[0];
+
+            let oblacnost = cloudy.getAttribute("name");
+            let vitr = windDirection.getAttribute("name") + " " + windSpeed.getAttribute("mps") + " m/s";
+            let teplota = temperature.getAttribute("value") + "°C";
+            let tlak = pressureTag.getAttribute("value") + " " + pressureTag.getAttribute("unit");
+
+            document.getElementById("weather").innerHTML = oblacnost + "<br>" + vitr + "<br>" + teplota + "<br>" + tlak;
         }
     };
 
     xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://www.yr.no/place/Czech_Republic/Zl%C3%ADn/Vizovice/forecast.xml", true);
-    //xhttp.setRequestHeader("Access-Control-Allow-Origin", "*");
-    //xhttp.setRequestHeader("Content-Type", "application/json");
     xhttp.send();
 
-    //return xhttp.responseText;
 }
 
